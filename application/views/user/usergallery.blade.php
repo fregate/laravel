f7237ff8c281e2b8c8e1efe2cae5e73956b0374a
A <fregate@ngs.ru>
2013-08-22 08:55:17 +0000

    gallery: show full image

--- application/views/user/usergallery.blade.php
+++ application/views/user/usergallery.blade.php
@@ -1,11 +1,82 @@
 @layout('templates.profile')
 
 @section('morelinks')
+<link rel="stylesheet" href="css/jquery.fileupload-ui.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css" />
+<script src="bundles/jupload/js/vendor/jquery.ui.widget.js"></script>
+<script src="bundles/jupload/js/jquery.iframe-transport.js"></script>
+<script src="bundles/jupload/js/jquery.fileupload.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+
 <script type="text/javascript">
 $(document).ready(function() {
   $("#liimgs").addClass("active");
+
+  $("#uploadfiles").fileupload({
+    url: "{{URL::base()}}/upload/xxx",
+    dataType: 'json',
+    acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+    minFileSize: 1,
+    done: function (e, data) {
+      console.log(data.result);
+      get_gallery_ajax(true);
+      $("#upload_waiting").hide();
+      $(".fileinput-button").removeAttr('disabled');
+    },
+    progressall: function (e, data) {
+      $("#upload_waiting").show();
+      $(".fileinput-button").attr('disabled', 'disabled');
+    }
+  });
 });
+
+var mconst = 40;
+function resize_dlg(dlgid) {
+  if( $( dlgid ).dialog( "isOpen")) {
+    $( dlgid ).dialog( "option", "height", window.innerHeight - mconst );
+    $( dlgid ).dialog( "option", "width", window.innerWidth - 2 * mconst );
+  }
+}
+
+$(window).resize(function(){
+  resize_dlg("#showimg");
+});
+
+$(function () {
+  $( "#showimg" ).dialog({
+    open: function() {
+      resize_dlg("#showimg");
+    },
+    autoOpen: false,
+    dialogClass: "dlgpos",
+    resizable:false,
+    modal:true,
+    draggable:false,
+    width:500,
+    height:500,
+    position: { my: "center top", at: "center top", of: window },
+    buttons: [
+    {
+      text: "Cancel",
+      click: function() {
+        $( this ).dialog( "close" );
+      }
+    } ]
+  });
+});
+
+function fullimage(idi) {
+$("#imgcontainer").empty();
+$("#imgcontainer").html("<img src='{{ URL::to("image") }}/" + idi + "'/>");
+  $( "#showimg" ).dialog("open");
+}
 </script>
+<style>
+.dlgpos {
+  margin:20px;
+  position:fixed;
+}
+</style>
 @endsection
 
 @section('profilesection')
@@ -16,7 +87,7 @@ $useradmin = !Auth::guest() && Auth::user()->has_any_role(array('admin', 'modera
 $images = $user->images()->paginate();
 foreach ($images->results as $img) {
 	// need to get row height through css class
-  echo "<div class='imgspan'><img src='"
+  echo "<div class='imgspan'><img onclick='fullimage(".$img->id.")' src='"
         . AuxImage::get_uri($img->id, AuxImage::get_thumb_attrs($img->id, 194, 200)) ."' title='[".$img->sx."x".$img->sy."]'></img>";
   if($useradmin) {
     echo '<div><a href="#" data-toggle="modal" class="confirm-delete red" data-idi="' . $img->id . '" >[x] remove</a></div>';
@@ -27,7 +98,7 @@ foreach ($images->results as $img) {
 echo '<div style="clear:both">' . $images->links() . "</div>";
 
 echo '<div id="removeimg" class="modal hide fade in prompts" style="display: none; ">
-    <div class="modal-header">  
+    <div class="modal-header">
         <a class="close" data-dismiss="modal">×</a>  
         <h3>Delete image?</h3>  
     </div>  
@@ -54,4 +125,8 @@ $('.confirm-delete').on('click', function(e) {
 </script>";
 
 ?>
+<div id="showimg" title="Show image" >
+<div id="imgcontainer"></div>
+</div>
+
 @endsection
